9080f01edaa20bdbc5967f0824d91fcdbe74a949
Mayra Kisch <mayrakisch@gmail.com>
2022-11-23 18:58:53 -0300

    correcoes gerais de bugs

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,12 +1,14 @@
+import debounce from './debounce.js';
+
 export default class ScrollAnima {
-  constructor(section) {
-    this.sections = document.querySelectorAll(section);
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
-  // Pega a distância de cada item em relação do
+  // Pega a distância de cada item em relação
   // ao topo do site
   getDistance() {
     this.distance = [...this.sections].map((section) => {
@@ -18,14 +20,14 @@ export default class ScrollAnima {
     });
   }
 
-  // verifica a distancia em cada objeto para
+  // Verifica a distância em cada objeto
   // em relação ao scroll do site
   checkDistance() {
-    this.distance.forEach((section) => {
-      if (window.pageYOffset > section.offset) {
-        section.element.classList.add('ativo');
-      } else if (section.element.classList.contains('ativo')) {
-        section.element.remove('ativo');
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
@@ -39,6 +41,7 @@ export default class ScrollAnima {
     return this;
   }
 
+  // Remove o event de scroll
   stop() {
     window.removeEventListener('scroll', this.checkDistance);
   }
